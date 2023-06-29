2fc2a363aa558eea6593b03fa2b4923db6c84a99
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-06-29 13:59:08 -0500

    Colorblind

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     // Start is called before the first frame update
     public void PlayMaze()
     {
         SceneManager.LoadScene("maze");
     }
+
+    public void ToggleColorblindMode()
+    {
+        if (colorblindMode.enabled)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+    }
+
 
     public void QuitMaze()
     {
